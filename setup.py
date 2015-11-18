f8f052412e7c36f06caf8604c547c89c9f8a5a8a
Yannic Kilcher <yannic.kilcher@gmail.com>
2015-11-18 10:57:01 +0100

    prettier setup

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+
 import sys
 
-package_dir = 'python2_noedit' if sys.version_info[0] < 3 else 'mldatasets'
+name = 'mldatasets'
+
+package_dir = 'python2_noedit' if sys.version_info[0] < 3 else name
 
 setup(
-    name='mldatasets',
+    name=name,
     version='0.0.1',
     packages=['.'],
-    package_dir={'': package_dir},
+    package_dir={'':package_dir},
     url='',
     license='MIT',
     author='yk',
